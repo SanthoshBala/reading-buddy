81efff9719e0c63593621958b78015212fc4b117
Santhosh Balasubramanian <santhoshbala@gmail.com>
2015-11-01 14:43:11 -0800

    loads webview of 8000hours.org with EditText for URL, and initializes TTS engine

--- ReadingBuddy/app/src/main/java/com/santhoshbala/readingbuddy/MainActivity.java
+++ ReadingBuddy/app/src/main/java/com/santhoshbala/readingbuddy/MainActivity.java
@@ -7,23 +7,40 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
 import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static int PERMISSIONS_REQUEST_ACCESS_CAMERA;
+    int TTS_ENGINE_STATUS = 0;
+
+    private TextToSpeech tts;
+
+    private class TTSInitListener implements OnInitListener {
+        public void onInit(int status) {
+            TTS_ENGINE_STATUS = 1;
+        }
+    }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -33,23 +50,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    private void readTextAsSpeech(CharSequence text) {
+        this.tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, (String) text);
+    }
 
+    private void initializeFab() {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(ContextCompat.checkSelfPermission(getApplicationContext(),
+                if (ContextCompat.checkSelfPermission(getApplicationContext(),
                         android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                     dispatchTakePictureIntent();
-                    
+
                 } else {
-                    if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
                             android.Manifest.permission.CAMERA)) {
                         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                         builder.setMessage(R.string.camera_permission_dialog_message)
@@ -64,6 +80,34 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void initializeWebView() {
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://80000hours.org/career-guide/basics/");
+    }
+
+    private void initializeTTSEngine() {
+        TTSInitListener initListener = new TTSInitListener();
+        this.tts = new TextToSpeech(getApplicationContext(), initListener);
+
+        this.tts.setLanguage(Locale.ENGLISH);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeWebView();
+
+        initializeFab();
+
+        initializeTTSEngine();
+
+        readTextAsSpeech("Hello, Reading Buddy!");
     }
 
     @Override
